1d3f5905d88a96e7b6f745303f16e281bc24885e
JensBrandels <brandelsjens@gmail.com>
2024-10-21 15:38:14 +0200

    loginpage styling done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import "../sass/LoginPage.scss";
+import LCKDLOGO from "../assets/Logo.svg";
+import Eyeseeyou from "../assets/eyecansee.svg";
+import Eyecantsee from "../assets/eyecantsee.svg";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="login-main-container">
+      <header>
+        <button>Sign up</button>
+      </header>
+      <div className="login-content">
+        <img src={LCKDLOGO} alt="" />
+        <h1>LCKD</h1>
+        <h2>KEEPING YOUR PASSWORDS SAFE</h2>
+      </div>
+      <form className="login-forms">
+        <label htmlFor="username">USERNAME</label>
+        <input type="text" name="username" id="" />
+        <label htmlFor="password">PASSWORD</label>
+        <input type="text" name="password" id="" />
+        <img src={Eyecantsee} alt="" />
+        <button type="submit">LET ME IN</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
