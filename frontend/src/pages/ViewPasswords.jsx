b7945083c8349cee2496867b551ce7dfaea9cc96
JensBrandels <brandelsjens@gmail.com>
2024-10-23 14:48:55 +0200

    manually added storedpsw branch

--- frontend/src/pages/ViewPasswords.jsx
+++ frontend/src/pages/ViewPasswords.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import "../sass/ViewPasswords.scss";
+import LogoHeader from "../components/logoComponent.jsx";
+import CopyLogo from "../assets/copylogo.svg";
+import Storedpsws from "../components/StorePswComponent.jsx";
+import { useNavigate } from "react-router-dom";
 
 const ViewPasswords = () => {
-  return <div>ViewPasswords</div>;
+  const navigate = useNavigate();
+  const handleClick = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <div className="viewPasswords-main-container">
+      <LogoHeader />
+      <div className="storedPasswords-box">
+        <p className="storedPassword-text">STORED PASSWORDS</p>
+        <div className="component-box">
+          <Storedpsws onClickAction={() => handleClick("/editlckd")} />
+        </div>
+      </div>
+      <div className="plainSight-box">
+        <p className="sight-text">PLAIN SIGHT</p>
+        <div>
+          <p className="sight-text-two">ranodiwdwadwa</p>
+          <img src={CopyLogo} alt="" />
+        </div>
+      </div>
+      <button onClick={() => handleClick("/newlckd")}>NEW LCKD</button>
+    </div>
+  );
 };
 
 export default ViewPasswords;
